3a493cfa76cb69055596c09ae41a3aa6a23249da
MonuRajpoot <monurajpoot978@gmail.com>
2026-03-26 22:05:26 +0530

    Added TokenType enum for tokenizer

---
+++ src/tokenizer/TokenType.java
@@ -0,0 +1,17 @@
+public enum TokenType {
+    // Keywords for bloop language
+    PUT, INTO, PRINT, IF, THEN, REPEAT, TIMES,
+
+    // Data types for language(bloop)
+    NUMBER, STRING, IDENTIFIER,
+
+    // Operators for languange(bloop)
+    PLUS, MINUS, MULTIPLY, DIVIDE,
+    GREATER, LESS, EQUAL,
+
+    // Symbols for bloop language
+    LPAREN, RPAREN,
+
+    // special Symbol
+    EOF
+}
